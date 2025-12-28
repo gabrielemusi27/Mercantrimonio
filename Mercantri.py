77e0e9a04ab895825b31747d5a83191b22711942
gabrielemusi27 <gabriele.musi27@gmail.com>
2025-12-28 20:18:18 +0100

    Update Mercantri.py

--- Mercantri.py
+++ Mercantri.py
@@ -86,7 +86,7 @@ else:
             if st.button("📊 GENERA REPORT FINALE"):
                 st.session_state.show_report = True
             
-            if st.button("🔄 Ricarica Carte/Tavoli da Google"):
+            if st.button("🔄 Ricarica Carte eTavoli"):
                 del st.session_state.df_tavoli
                 del st.session_state.df_carte
                 st.rerun()
