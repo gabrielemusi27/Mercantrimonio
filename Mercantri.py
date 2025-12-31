043f076605c41ab0777cc7a74f22021243a1a0ce
gabrielemusi27 <gabriele.musi27@gmail.com>
2025-12-31 14:27:41 +0100

    Update Mercantri.py

--- Mercantri.py
+++ Mercantri.py
@@ -131,7 +131,7 @@ else:
             if st.button("📊 GENERA REPORT FINALE"):
                 st.session_state.show_report = True
 
-            if st.button("🐷 Reset dati (Maialino)"):
+            if st.button("🐷 Premi per elevare la vita di un povero maialino indifeso!"):
                 del st.session_state.df_tavoli
                 del st.session_state.df_carte
                 st.rerun()
